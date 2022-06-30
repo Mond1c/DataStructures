f61c40f9e9e2ca334f2fc87868f0524ec6911d92
Mond1c <kornilovichmihail@gmail.com>
2022-06-30 12:11:16 +0300

    :q X

--- c/main.c
+++ c/main.c
@@ -8,6 +8,7 @@ int main(void) {
     vector_t* new_vec = vector_copy(vec);
     vector_free(vec);
     vector_print(new_vec);
+    printf("%d\n", vector_get_element(new_vec, 3));
     vector_free(new_vec);
     return 0;
 }
